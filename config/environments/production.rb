0cea673af950fbf533b4c348e472fcfb30bf01a5
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-08-25 14:03:26 -0700

    Bug: Email rendering fails without a default url for links
            This adds it to the production config. It already exists in the
            other environment configurations.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,10 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = {
+    host: 'hhs-acf-uc-dashboard.apps.cloud.gov',
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
