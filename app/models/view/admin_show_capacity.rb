779f75be352b68a829b4254a3d9ab74ddc8a9655
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-09-07 12:41:13 -0700

    View handles showing errors on capicity invalidation

--- app/models/view/admin_show_capacity.rb
+++ app/models/view/admin_show_capacity.rb
@@ -23,6 +23,18 @@ module View
       @audit_logs ||= @logs.map{ |log| LogPresenter.new(log) }
     end
 
+    def errors?
+      !capacity.errors.empty?
+    end
+
+    def error_heading
+      'Some capacity values were invalid:'
+    end
+
+    def error_messages
+      capacity.errors.full_messages
+    end
+
     class LogPresenter
       attr_reader :model
 
