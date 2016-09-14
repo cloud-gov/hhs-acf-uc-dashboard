c57a4ef4dedfd3078ff28d06c6092a1c132f4927
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-09-13 17:19:16 -0700

    Production bug: constant not loading correctly (ActiveSupport foolery)

--- app/models/view/model_error.rb
+++ app/models/view/model_error.rb
@@ -1,5 +1,16 @@
 module View
-  class ModelError < Struct.new(:model, :heading)
+  class ModelError
+    attr_reader :model
+
+    def initialize(model, heading)
+      @model = model
+      @heading = heading
+    end
+
+    def heading
+      @heading || 'Some of your data was invalid'
+    end
+
     def errors?
       !model.errors.empty?
     end
