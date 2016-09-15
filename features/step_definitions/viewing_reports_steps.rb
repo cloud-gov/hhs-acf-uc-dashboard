24b385efb20e0c89c5dc93750e3946e5d5a61435
Kane Baccigalupi <kane.baccigalupi@gsa.gov>
2016-09-15 07:36:29 -0700

    Failing wip feature for first daily report feature

---
+++ features/step_definitions/viewing_reports_steps.rb
@@ -0,0 +1,31 @@
+Given(/^I am a user with permission to view the dashboard$/) do
+  step "I have signed in as an authenticated 'Admin' user"
+end
+
+Given(/^capacity numbers were locked yesterday, but not today$/) do
+  @yesterdays_capacity = Capacity.create({
+    date: Date.today - 1,
+    funded: 100,
+    reserve: 200,
+    activated: 300,
+    unavailable: 400,
+    status: 'locked'
+  })
+
+  @todays_capacity = Capacity.create({
+    date: Date.today,
+    funded: 1000,
+    reserve: 2000,
+    activated: 3000,
+    unavailable: 4000,
+    status: 'unlocked'
+  })
+end
+
+When(/^I visit 'Daily' report page$/) do
+  click_on "Daily"
+end
+
+Then(/^I should see yesterday's daily report$/) do
+  expect(page).to have_content(@yesterdays_capacity.date.strftime('%m/%d/%y'))
+end
---
+++ features/viewing_reports.feature
@@ -0,0 +1,11 @@
+Feature:
+  As an user with access to reports
+  I want to see daily data
+  So that I can make important decisions about the program and funding
+
+  @wip
+  Scenario: Viewing daily report when today's daily capacity numbers are unlocked
+    Given I am a user with permission to view the dashboard
+    And capacity numbers were locked yesterday, but not today
+    When I visit 'Daily' report page
+    Then I should see yesterday's daily report
